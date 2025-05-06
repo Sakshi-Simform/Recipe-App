aa658337ce4bb2156fdeb5e8b368973c688d0b4b
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-06 10:35:54 +0530

    feature: utility import and remove interface

---
+++ src/components/RecipeList.tsx
@@ -0,0 +1,22 @@
+import  { FC } from 'react';
+import RecipeCard from '../components/RecipeCard';
+import { Recipe } from '../utilities/utility';
+
+interface RecipeListProps {
+  recipes: Recipe[];
+}
+
+const RecipeList: FC<RecipeListProps> = ({ recipes }) => {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          recipe={recipe}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
