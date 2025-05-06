5b2e160f80c63186075f2656f96459a83b2e916d
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-06 10:52:38 +0530

    made code minimal in app main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,17 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RecipeList from './RecipeList';
-import SearchBar from './searchBar';
-import RecipeDetails from './RecipeDetails';
+import React from 'react';
 import './App.css';
-import { sampleRecipes } from './sampleRecipe';
-import { Recipe } from './RecipeCard'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import RecipeDetails from './components/RecipeDetails';
 
 const App: React.FC = () => {
-  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>(sampleRecipes);
-
-  const handleSearch = (searchTerm: string) => {
-    const filtered = sampleRecipes.filter((recipe) =>
-      recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      recipe.ingredients.some((ingredient) => 
-        ingredient.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-    setFilteredRecipes(filtered);
-  };
-
   return (
     <Router>
       <div className="app">
-        <header>
-          <div className='left-logo'>
-            <img
-              src='https://img.freepik.com/premium-vector/simple-clean-modern-spoon-restaurant-logo-black-white-color-vector-illustration_969863-11106.jpg'
-              className='logo-image'
-              alt='logo'
-            />
-            <h1 id='app-name'>CookBook</h1>
-          </div>
-          <SearchBar onSearch={handleSearch} />
-        </header>
-
-        <main>
-          <Routes>
-            <Route path="/" element={<RecipeList recipes={filteredRecipes} />} />
-            <Route path="/recipe/:id" element={<RecipeDetails />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
       </div>
     </Router>
   );
