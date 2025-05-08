0a01315d44ca9f3d09cb0781dfe05bdeb82f7b27
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-08 13:53:50 +0530

    feature: add new home file

---
+++ src/components/Home.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import RecipeList from './RecipeList';
+import { sampleRecipes } from './sampleRecipe';
+import { Recipe } from '../utilities/utility';
+
+const HomePage: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>(sampleRecipes);
+
+  useEffect(() => {
+    const filtered = sampleRecipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      recipe.ingredients.some((ingredient) =>
+        ingredient.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredRecipes(filtered);
+  }, [searchTerm]);
+
+  return (
+    <>
+      <Header onSearch={setSearchTerm} />
+      <RecipeList recipes={filteredRecipes} />
+    </>
+  );
+};
+
+export default HomePage;
