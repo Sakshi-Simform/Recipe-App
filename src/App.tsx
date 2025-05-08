4d187b9e1e68ba09753f775cbc4904f4798fd425
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-08 13:54:47 +0530

    feature: remove route for detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import RecipeDetails from './components/RecipeDetails';
+import HomePage from './components/Home';
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
