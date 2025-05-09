e0686a35a0a78dd989e3c93570c96e242d04a139
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-09 12:12:33 +0530

    feature: move home file in pages folder

---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import RecipeList from '../components/RecipeList';
+import { sampleRecipes } from '../MockData/sampleRecipe';
+import { Recipe } from '../utilities/types';
+
+const HomePage: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredRecipes, setFilteredRecipes] = useState<Array<Recipe>>(sampleRecipes);
+
+  useEffect(() => {
+    const filtered = sampleRecipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      recipe.ingredients.some((ingredient) =>
+        ingredient.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredRecipes(filtered);
+  }, [searchTerm]);
+
+  return (
+    <>
+      <Header onSearch={setSearchTerm} />
+      <RecipeList recipes={filteredRecipes} />
+    </>
+  );
+};
+
+export default HomePage;
+
+
+
