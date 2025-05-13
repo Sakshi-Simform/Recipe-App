359f4b6edf80327d65a6611259947fd47b806007
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-13 18:09:57 +0530

    feature: change svg file for calories

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,5 +1,6 @@
-import '../styles/RecipeCard.css'
 import { Recipe } from '../utilities/types';
+import calories from '/assets/calories.svg';
+import '../styles/RecipeCard.css'
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -33,7 +34,10 @@ export const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
           <div className="recipe-info-box">
             <div className="calories-section">
               <div className="calories">
-              <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C16.4183 21 20 17.6439 20 13.504C20 9.76257 17.9654 6.83811 16.562 5.44436C16.3017 5.18584 15.8683 5.30006 15.7212 5.63288C14.9742 7.3229 13.4178 9.75607 11.4286 9.75607C10.1975 9.92086 8.31688 8.86844 9.83483 3.64868C9.97151 3.17868 9.46972 2.80113 9.08645 3.11539C6.9046 4.90436 4 8.51143 4 13.504C4 17.6439 7.58172 21 12 21Z" fill="#1C274C"></path> </g></svg>
+                <img 
+                src={calories}
+                alt='calories'
+                />
                 <p className='calorie-display'>{recipe.calories} kcal</p>
               </div>
             </div>
