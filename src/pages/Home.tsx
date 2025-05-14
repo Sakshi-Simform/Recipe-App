7dcd78aef8e44e7f8a90e02c3f4446a3a2e9799a
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-14 11:46:01 +0530

    feature: add type to import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Header } from '../components/Header';
 import { RecipeList } from '../components/RecipeList';
 import { sampleRecipes } from '../MockData/sampleRecipes';
-import { Recipe } from '../utilities/types';
+import type { Recipe } from '../types/recipe.types';
 
 export const HomePage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
