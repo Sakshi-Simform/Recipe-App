5cc5aafcbec2c0623132f39012ae5bd924fb92c0
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-14 12:04:14 +0530

    feature: change search code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Header } from '../components/Header';
 import { RecipeList } from '../components/RecipeList';
 import { sampleRecipes } from '../MockData/sampleRecipes';
@@ -6,15 +6,9 @@ import type { Recipe } from '../types/recipe.types';
 
 export const HomePage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [filteredRecipes, setFilteredRecipes] = useState<Array<Recipe>>(sampleRecipes);
 
-  useEffect(() => {
-    const filtered = sampleRecipes.filter((recipe) =>
-      recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredRecipes(filtered);
-  }, [searchTerm]);
-
+  const filteredRecipes: Recipe[] = sampleRecipes.filter((recipe) =>
+  recipe.title.toLowerCase().includes(searchTerm.toLowerCase()))
   return (
     <>
       <Header onSearch={setSearchTerm} />
