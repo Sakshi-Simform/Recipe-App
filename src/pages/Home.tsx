e9572cca0471d851b3ae53051bc03a3984ede9d3
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-14 17:51:08 +0530

    feature: add validation for search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,23 @@ import { Header } from '../components/Header';
 import { RecipeList } from '../components/RecipeList';
 import { sampleRecipes } from '../MockData/sampleRecipes';
 import type { Recipe } from '../types/recipe.types';
+import styles from '../styles/SearchBar.module.css'
 
 export const HomePage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   const filteredRecipes: Recipe[] = sampleRecipes.filter((recipe) =>
-  recipe.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <Header onSearch={setSearchTerm} />
-      <RecipeList recipes={filteredRecipes} />
+      {!filteredRecipes.length ? (
+        <p className={styles.searchkeyword}>No recipes found for "{searchTerm}". Please try a different keyword.</p>
+      ) : (
+        <RecipeList recipes={filteredRecipes} />
+      )}
     </>
   );
 };
