46b6b6c68c535aec3618d6e6e226be75d24f2b20
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-15 11:27:41 +0530

    feature: add file path for navigartion

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {HomePage} from './pages/Home';
 import styles from './styles/App.module.css'
+import { RecipeDetails } from './components/RecipeDetails';
 
 export const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ export const App: React.FC = () => {
       <div className={styles.app}>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetails/>} />
         </Routes>
       </div>
     </Router>
