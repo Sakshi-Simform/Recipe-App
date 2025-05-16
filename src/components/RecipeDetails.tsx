8b053e9043a6242ae280406ecee70bc9f5c6d582
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-16 11:06:57 +0530

    feature:change class name,parent selector,hover effect

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -14,31 +14,31 @@ export const RecipeDetails: React.FC = () => {
       <Header onSearch={() => {}} showSearch={false} /> 
 
       {/* recipe-details */}
-      <div className={styles.recipedetails}>
-        <div className={styles.recipecontainer}>
+      <div className={styles.recipeDetails}>
+        <div className={styles.recipeContainer}>
           {recipe ? (
             <>
-              <div className={styles.detailimage}>
-                <img className={styles.recipedetailsimg} src={recipe.image} alt={recipe.title} />
+              <div className={styles.detailImage}>
+                <img className={styles.recipeDetailsImg} src={recipe.image} alt={recipe.title} />
               </div>
               <div className={styles.info}>
-                <h2 className={styles.details}>{recipe.title}</h2>
-                <p className={styles.details}><strong>Description:</strong> {recipe.shortDescription}</p>
-                <p className={styles.details}><strong>Calories:</strong> {recipe.calories} kcal</p>
-                <p className={styles.details}><strong>Rating:</strong> {recipe.rating}/5</p>
+                <h2 className={styles.titleDetails}>{recipe.title}</h2>
+                <p className={styles.description}><strong>Description:</strong> {recipe.shortDescription}</p>
+                <p className={styles.description}><strong>Calories:</strong> {recipe.calories} kcal</p>
+                <p className={styles.description}><strong>Rating:</strong> {recipe.rating}/5</p>
                 <h3 className={styles.details}>Ingredients</h3>
                 <ul>
                   {recipe.ingredients.map((item, index) => (
-                    <li className={styles.details} key={index}>{item}</li>
+                    <li className={styles.listDetails} key={index}>{item}</li>
                   ))}
                 </ul>
 
                 <h3 className={styles.details}>Instructions</h3>
-                <p className={styles.details}>{recipe.instructions}</p>
+                <p className={styles.instructions}>{recipe.instructions}</p>
               </div>
             </>
           ) : (
-            <div className="recipedata">No recipe data found.</div>
+            <div className={styles.recipeData}>No recipe data found.</div>
           )}
         </div>
       </div>
