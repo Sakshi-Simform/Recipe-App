12b51baaa0356f194e7404339f6557d718a06751
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-19 11:25:49 +0530

    feature: change rendering for header

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { Header } from './Header';
+import { MainLayout } from '../components/MainLayout';
 import { Recipe } from '../types/recipe.types';
-import styles from '../styles/RecipeDetails.module.css'
+import styles from '../styles/RecipeDetails.module.css';
 
 export const RecipeDetails: React.FC = () => {
   const { state } = useLocation();
   const recipe: Recipe | undefined = state;
 
   return (
-    <>
-      {/* Only display the logo, not the search bar */}
-      <Header onSearch={() => {}} showSearch={false} /> 
-
-      {/* recipe-details */}
+    <MainLayout showSearch={false}>
       <div className={styles.recipeDetails}>
         <div className={styles.recipeContainer}>
           {recipe ? (
             <>
               <div className={styles.detailImage}>
-                <img className={styles.recipeDetailsImg} src={recipe.image} alt={recipe.title} />
+                <img
+                  className={styles.recipeDetailsImg}
+                  src={recipe.image}
+                  alt={recipe.title}
+                />
               </div>
+
               <div className={styles.info}>
                 <h2 className={styles.titleDetails}>{recipe.title}</h2>
-                <p className={styles.description}><strong>Description:</strong> {recipe.shortDescription}</p>
-                <p className={styles.description}><strong>Calories:</strong> {recipe.calories} kcal</p>
-                <p className={styles.description}><strong>Rating:</strong> {recipe.rating}/5</p>
+                <p className={styles.description}>
+                  <strong>Description:</strong> {recipe.shortDescription}
+                </p>
+                <p className={styles.description}>
+                  <strong>Calories:</strong> {recipe.calories} kcal
+                </p>
+                <p className={styles.description}>
+                  <strong>Rating:</strong> {recipe.rating}/5
+                </p>
+
                 <h3 className={styles.details}>Ingredients</h3>
                 <ul>
                   {recipe.ingredients.map((item, index) => (
-                    <li className={styles.listDetails} key={index}>{item}</li>
+                    <li className={styles.listDetails} key={index}>
+                      {item}
+                    </li>
                   ))}
                 </ul>
 
@@ -42,6 +52,6 @@ export const RecipeDetails: React.FC = () => {
           )}
         </div>
       </div>
-    </>
+    </MainLayout>
   );
 };
